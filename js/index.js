8c67f912174b4c36a6ebfc1b2d8b6db8fcc1130c
HelloDr <1534538724@qq.com>
2019-01-16 17:32:03 +0800

    2019/1/16 first  commit

--- js/index.js
+++ js/index.js
@@ -1,4 +1,10 @@
+//判断本地存储中是否存放了各个奖品的数量
+if (localStorage.getItem('arrNum') == null) {
+  //将各个奖品的数量放入本地存储
+  localStorage.setItem('arrNum', [3, 5, 20, 1, 2, 1, 3, 20, 5, 2]);
+}
 $(function () {
+  $(".covers1").hide();
   // 转盘指针添加点击事件
   $('.pointer').click(function () {
     // 调用转盘函数
@@ -34,7 +40,7 @@ $(function () {
     });
   };
   var bRotate = false;
-
+  //转盘旋转函数
   var rotateFn = function (awards, angles, txt) {
     bRotate = !bRotate;
     $('#rotate').stopRotate();
@@ -43,44 +49,83 @@ $(function () {
       animateTo: angles + 2825,
       duration: 8000,
       callback: function () {
-        $(".cover,.covers2").show();
+        $(".covers2").show();
         $(".covers2 .covers_font").html("<span style='color#000'>恭喜你抽中:</span><br>" + txt);
         if (angles == 72 || angles == 252) {
           $(".covers2 .covers_font").text(txt);
+          //$(".covers_btn2").hide();
         }
         bRotate = !bRotate;
       }
     })
   };
 
-  var time = 2;
+  //抽奖次数
+  var time = 1;
 
   function Rotate() {
     //存储转盘奖品
-    var arr = ['30天免费借阅', '20天免费借阅', '谢谢参与', '免费借阅四次','免费借阅三次', '佳农探趣生态园大礼包', '免费借阅两次',
-      '谢谢参与', '免费借阅一次', '40天免费借阅'];
+    var arr = ['30天免费借阅', '20天免费借阅', '谢谢参与', '免费借阅四次', '免费借阅三次', '佳农探趣生态园大礼包', '免费借阅两次',
+     '谢谢参与', '免费借阅一次', '40天免费借阅'];
+    //从本地存储获取奖品数量
+    var arr1 = (localStorage.getItem('arrNum')).split(',');
     // 防止多次点击
     if (bRotate)return;
     //抽奖次数减一
     time--;
     //判断抽奖次数
-    if (time <= 0) {
+    if (time < 0) {
       //提示抽奖次数已用完
-      $(".covers3").show();
-    } else {
-      //var item=0;
-      //产生转盘随机数
-      var item = parseInt(Math.random() * (10 - 0 + 0) + 0);
-      console.log(item);
+      $(".covers1").show();
+    }
+    else {
+      //var item = 9;
+      //奖品概率
+      //var item = parseInt(Math.random() * (10 - 0 + 0) + 0);
+      //调用权重函数
+      var item = weightRandom(1);
       //循环奖品数组
       for (var i = 0; i < arr.length; i++) {
-        console.log(arr[i],i);
+        //var item = ((arr1[i]/62).toFixed(2));
         //根据随机数概率判断所中奖品
-        switch (item){
-          //调用转盘函数
-          case i : rotateFn(i,i*36,arr[i]);
-            break;
+        console.log(i,arr[i], item);
+        //判断该奖品是否已抽完
+        if (arr1[i] == 0) {
+          //将该奖品值设为空
+          arr.splice(i, 1, '');
         }
+        //并把每次改变奖品数量，放入本地存储
+        localStorage.setItem('arrNum', arr1);
+        //判断抽中该项奖品是否还有
+        if (arr[item] != "") {
+          switch (item) {
+            //调用转盘函数
+            case i :
+              rotateFn(i,i * 36, arr[i]);
+              //用户每抽一次奖品数量减一
+              arr1.splice(i, 1, arr1[i] > 0 ? arr1[i] - 1 : 0);
+              break;
+          }
+        } else {//抽中的该项奖品已没有，弹框提示用户谢谢参与
+          bRotate = !bRotate;
+          $('#rotate').stopRotate();
+          $('#rotate').rotate({
+            angle: 0,
+            animateTo: (item * 36) + 2825,
+            duration: 8000,
+            callback: function () {
+              if (item == 2 || item == 7) {
+                $(".covers2 .covers_font").text("谢谢参与");
+                //$(".covers_btn2").hide();
+                $(".covers2").show();
+              } else {
+                $(".covers3").show();
+              }
+              bRotate = !bRotate;
+            }
+          })
+        }
+
         // switch (item) {
         //   case 0:
         //     rotateFn(0, 0, '30天免费借阅');
@@ -123,6 +168,61 @@ $(function () {
         //     break;
         // }
       }
+      //console.log(arr);
+      // if (arr == null) {
+      //   $(".covers3").show();
+      // }
+      /**
+       *
+       * @param array 存放奖品的函数
+       * @returns {boolean} 奖品是否全部抽完
+       */
+      function isAllEqual(array) {
+        if (array.length > 0) {
+          return !array.some(function (value, index) {
+            return value !== array[0];
+          });
+        } else {
+          return true;
+        }
+      }
+
+      /**
+       * 权重函数
+       * @param curValue
+       * @returns {*}
+       */
+      function weightRandom(curValue) {
+        var randomConfig = [{id:"0",weight:3},{id:"1",weight:5},{id:"2",weight:20},
+          {id:"3",weight:1},{id:"4",weight:2},{id:"5",weight:1},{id:"6",weight:3},
+          {id:"7",weight:20},{id:"8",weight:5},{id:"9",weight:2}];
+        var randomList = [];
+        for (var item in randomConfig) {
+          for (var j = 0; j < randomConfig[item].weight; j++) {
+            randomList.push(randomConfig[item].id);
+          }
+        }
+        var randomValue = randomList[Math.floor(Math.random() * randomList.length)];
+        if (curValue != 0) {
+          while (randomValue == curValue ) {
+            randomValue  = randomList[Math.floor(Math.random() * randomList.length)];
+          }
+        }
+        console.log(randomList);
+        return Number(randomValue) ;
+      }
+
+      //判断奖品是否全部抽完
+      if (isAllEqual(arr)) {
+        //停止转盘旋转
+        $('#rotate').stopRotate();
+        //提示用户奖品已全部抽完
+        $(".covers1 .covers_font").text("所有奖品已抽完");
+        $(".covers_btn2").hide();
+        $(".covers1").show();
+      }
     }
   }
+  
+  
 });
